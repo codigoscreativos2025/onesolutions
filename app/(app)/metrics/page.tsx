3eff6374603e0ea0647fd06755fba91e1a065889
¨hendelberth¨ <hendelbeeapinoza@gmail.com>
2026-08-27 11:33:24 -0400

    fix(metrics): fix missing closing brackets for AdminExtraViews component

--- app/(app)/metrics/page.tsx
+++ app/(app)/metrics/page.tsx
@@ -162,6 +162,9 @@ function AdminExtraViews({ data }: { data: AdminMetrics }) {
           </span>
         </div>
       </div>
+    </>
+  );
+}
 
 function AdminView({ data }: { data: AdminMetrics }) {
   return (
